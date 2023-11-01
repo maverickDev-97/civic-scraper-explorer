bfce5d170365b77e5c1da46f040c99b2e76cc82f
Dmytro Pryshliak <dmytropryshliak@DPRYSHLIAK2-MAC.local>
2023-11-01 15:48:47 +0200

    Dockerizing the app

--- scrapers.js
+++ scrapers.js
@@ -2,10 +2,16 @@ import puppeteer from "puppeteer";
 
 export const scrap = async () => {
   const browser = await puppeteer.launch({
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
     args: [
       "--no-sandbox",
+      "--disable-setuid-sandbox",
       "--headless",
       "--disable-gpu",
+      "--no-zygote",
       "--window-size=1920x1080",
     ],
   });
